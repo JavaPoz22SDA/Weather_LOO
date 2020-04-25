fbf538b6017f05918e2f89a497e95d6cab1cd50d
Lukasz Olas <lukaszolas@gmail.com>
2020-04-25 13:38:39 +0200

    save weather obj to json

---
+++ src/main/java/pl/sda/weather/Main.java
@@ -0,0 +1,32 @@
+package pl.sda.weather;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Location location = new Location("Poznan", "Poland");
+        Request request = new Request("City", "Poznan,Poland");
+
+        Weather weather = new Weather(23.23, "Poznan",request,location);
+        System.out.println(weather.getTemperature());
+        weather.setTemperature(34.33);
+        System.out.println(weather.getTemperature());
+
+        mapper.writeValue(new File("data.json"), weather);
+
+        Weather readWeather =  mapper.readValue(new File("data.json"),
+                Weather.class);
+        System.out.println(readWeather.getCity());
+
+
+    }
+
+
+}
